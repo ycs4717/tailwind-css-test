376acafe43f4e54599e5ce4d4596d45c3ec8a14b
ycs4717 <123321032+ycs4717@users.noreply.github.com>
2023-09-01 14:56:08 +0900

    헤더 끝

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <header className="text-gray-700 border-b border-gray-200">
+      {/* md ... md以上の時にcssを適用 */}
+      <div className="container flex mx-auto  p-5 flex-col md:flex-row  items-center">
+        <a href="#" className="font-medium text-gray-900 mb-4 md:mb-0">
+          <span className="ml-3 text-xl">ShinCode</span>
+        </a>
+        <nav className="md:ml-auto text-base">
+          <a href="#home" className="mr-5 hover:text-blue-400 duration-300">
+            Home
+          </a>
+          <a href="#about" className="mr-5 hover:text-blue-400 duration-300">
+            About
+          </a>
+          <a href="#skill" className="mr-5 hover:text-blue-400 duration-300">
+            Skills
+          </a>
+          <a href="#blog" className="mr-5 hover:text-blue-400 duration-300">
+            Blog
+          </a>
+          <a href="#contact" className="hover:text-blue-400 duration-300">
+            Contact
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default App;
