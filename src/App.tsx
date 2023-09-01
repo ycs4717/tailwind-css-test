0c4da35664b639890c256e9f6305374dfc1eb328
ycs4717 <123321032+ycs4717@users.noreply.github.com>
2023-09-01 18:30:47 +0900

    My Skills 완성

--- src/App.tsx
+++ src/App.tsx
@@ -49,12 +49,11 @@ const App = () => {
             </button>
           </div>
           <div className="md:w-1/2 lg:max-w-lg w-5/6">
-            <img src="./img/icon.gif" alt="" />
+            <img src="./img/icon1.gif" alt="" />
           </div>
         </div>
       </section>
       {/*About Me*/}
-
       <section id="about" className="text-gray-700 border-t border-gray-200">
         <div className="container px-5 py-24 mx-auto">
           <div className="text-center mb-20">
@@ -207,6 +206,59 @@ const App = () => {
                       <path d="M5 12h14M12 5l7 7-7 7"></path>
                     </svg>
                   </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/*My Skills*/}
+      <section id="skill" className="text-gray-700 border-t border-gray-200">
+        <div className="flex container px-5 py-24 mx-auto flex-wrap">
+          <div className="rounded w-full mb-10 lg:w-1/2 lg:mb-0">
+            <img src="./img/icon2.gif" alt="" />
+          </div>
+          <div className="lg:py-6 lg:pl-12 lg:text-left lg:w-1/2 w-full">
+            <h1 className="sm:text-3xl text-2xl font-medium text-gray-900 mb-10">
+              My Skills
+            </h1>
+            <div className="">
+              <div className="w-full">
+                <h2>HTML</h2>
+                <div className="shadow w-full bg-green-100 mt-2">
+                  <div
+                    className="bg-green-600 text-xs leading-none py-1 text-center text-white"
+                    style={{ width: "85%" }}
+                  >
+                    85%
+                  </div>
+                </div>
+                <h2 className="pt-5">CSS</h2>
+                <div className="shadow w-full bg-green-100 mt-2">
+                  <div
+                    className="bg-green-600 text-xs leading-none py-1 text-center text-white"
+                    style={{ width: "80%" }}
+                  >
+                    80%
+                  </div>
+                </div>
+                <h2 className="pt-5">Javascript</h2>
+                <div className="shadow w-full bg-green-100 mt-2">
+                  <div
+                    className="bg-green-600 text-xs leading-none py-1 text-center text-white"
+                    style={{ width: "65%" }}
+                  >
+                    65%
+                  </div>
+                </div>
+                <h2 className="pt-5">Tailwindcss</h2>
+                <div className="shadow w-full bg-green-100 mt-2">
+                  <div
+                    className="bg-green-600 text-xs leading-none py-1 text-center text-white"
+                    style={{ width: "75%" }}
+                  >
+                    75%
+                  </div>
                 </div>
               </div>
             </div>
